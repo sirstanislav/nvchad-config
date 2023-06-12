becc09de2faeb553b317b9fbc803964c0f5b56aa
Stanislav Sorochan <stas.sorochan@gmail.com>
2023-06-12 11:18:34 +0200

    refactor: treesitter

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,11 +1,66 @@
 local plugins = {
+  --typescript-language-server
   {
     "jose-elias-alvarez/typescript.nvim",
   },
+
+  --junp between tmux and vim panes
   {
     "christoomey/vim-tmux-navigator",
     lazy = false,
   },
+
+  {
+    "neovim/nvim-lspconfig",
+
+    dependencies = {
+      "jose-elias-alvarez/null-ls.nvim",
+      config = function()
+        require "custom.configs.null-ls"
+      end,
+    },
+
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "eslint_d",
+        "typescript-language-server",
+        "angular-language-server",
+        "lua-language-server",
+        "html-lsp",
+        "prettierd",
+        "stylua",
+        "json-lsp",
+      },
+    },
+  },
+
+  {
+    "junegunn/gv.vim",
+    lazy = false,
+  },
+
+  {
+    "tpope/vim-fugitive",
+    lazy = false,
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    lazy = false,
+    config = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
+
   {
     "nvim-telescope/telescope-project.nvim",
     config = function()
@@ -27,6 +82,8 @@ local plugins = {
       }
     end,
   },
+
+  --code highlighting
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
@@ -49,52 +106,6 @@ local plugins = {
         "zig",
       },
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-
-    dependencies = {
-      "jose-elias-alvarez/null-ls.nvim",
-      config = function()
-        require "custom.configs.null-ls"
-      end,
-    },
-
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    opts = {
-      ensure_installed = {
-        "eslint_d",
-        "typescript-language-server",
-        "angular-language-server",
-        "lua-language-server",
-        "html-lsp",
-        "prettierd",
-        "stylua",
-        "json-lsp",
-      },
-    },
-  },
-  {
-    "junegunn/gv.vim",
-    lazy = false,
-  },
-  {
-    "tpope/vim-fugitive",
-    lazy = false,
-  },
-  {
-    "iamcco/markdown-preview.nvim",
-    lazy = false,
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
   },
 }
 
