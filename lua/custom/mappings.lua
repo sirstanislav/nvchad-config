1f5497bc95119586d4ac5ddbd19fab2c2574cec7
Stanislav Sorochan <ctacbarada@Stanislavs-MacBook-Air.local>
2023-08-10 16:18:18 +0200

    feat: navigation mappings between tmux and vim

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -16,6 +16,11 @@ M.general = {
     --   end,
     --   "󰂵 Toggle transparency",
     -- },
+
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "Window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "Window right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "Window down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "Window up" },
   },
 }
 
